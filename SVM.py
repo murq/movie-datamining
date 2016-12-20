655c595bda775589befa4b8733fbaaf3f6292fc7
Biel <bpierasmorell@gmail.com>
2016-12-20 16:53:27 +0100

    Added dataset normalization

--- SVM.py
+++ SVM.py
@@ -1,6 +1,7 @@
 import pandas
 import numpy
 import sklearn
+from sklearn import preprocessing
 from sklearn import svm
 from sklearn.grid_search import GridSearchCV
 
@@ -12,13 +13,17 @@ data = numpy.array(csvFile)
 X = data[:,1:-1]
 y = data[:,-1]
 
-poly_params = {'C':numpy.logspace(-3,4,8), 'kernel':['poly'],'degree':range(1,4)} #C=[0.001,0.01,0.1,1,10,100,1000,10000], degree=[1,2,3,4]
-poly_grid = GridSearchCV(svm.SVC(), param_grid=poly_params, cv=20)
-poly_grid.fit(X,y)
-print "Best Params=",poly_grid.best_params_, "Accuracy=", poly_grid.best_score_
+X_scaled = preprocessing.scale(X)
 
 rbf_params = {'C':numpy.logspace(-3,4,8), 'kernel':['rbf'],'gamma':numpy.logspace(-3,4,8)}
 rbf_grid = GridSearchCV(svm.SVC(), param_grid=rbf_params, cv=20)
-rbf_grid.fit(X,y)
+print 'Starting rbf fit...'
+rbf_grid.fit(X_scaled,y)
 print "Best Params=",rbf_grid.best_params_, "Accuracy=", rbf_grid.best_score_
+
+poly_params = {'C':numpy.logspace(-3,4,8), 'kernel':['poly'],'degree':range(1,4)} #C=[0.001,0.01,0.1,1,10,100,1000,10000], degree=[1,2,3,4]
+poly_grid = GridSearchCV(svm.SVC(), param_grid=poly_params, cv=20)
+print 'Starting poly fit...'
+poly_grid.fit(X_scaled,y)
+print "Best Params=",poly_grid.best_params_, "Accuracy=", poly_grid.best_score_
 
